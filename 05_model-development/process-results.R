5deb800a600e54f6f04aaeaf95838c359a981ae5
tomravalde <tomravalde@hotmail.com>
2016-06-23 17:35:47 +0100

    More work on chapter draft.

--- 05_model-development/process-results.R
+++ 05_model-development/process-results.R
@@ -17,10 +17,10 @@ library(ggplot2)
 network <- read.csv("network-summary.csv")
 stats <- read.csv("benchmark-results.csv")
 
-## Merge
+## Merge with data which describes the network (i.e. number of processes, resource, transport etc.)
 data <- merge(network, stats)
 
-##
+## Remove some statistics
 data <- filter(data, !stat_unordered %in% c("instructions_nonlinear",
 					    "iterations",
 					    "nodes",
@@ -29,70 +29,57 @@ data <- filter(data, !stat_unordered %in% c("instructions_nonlinear",
 					    "time_toGenerate",
 					    "elements_nonzero"))
 
+###--------------------------------------------------
+### Repeat the obj_est and time_toGenerate data, removing the outlying values at 6 and 8 conversion processes
+###--------------------------------------------------
+
+data_reduced <- filter(data, !conversion %in% c(6,8))
+
+outliersRemoved_objVal <- filter(data_reduced, stat_unordered=="obj_val")
+outliersRemoved_objVal$stat_unordered <- "obj_val_reduced"
+
+outliersRemoved_solTime <- filter(data_reduced, stat_unordered=="time_toSolve")
+outliersRemoved_solTime$stat_unordered <- "time_toSolve_reduced"
+
+data <- rbind(data, outliersRemoved_objVal, outliersRemoved_solTime)
+
+###--------------------------------------------------
+### Prepare data for plotting
+###--------------------------------------------------
+
 ## Create display names for the model statistics
 eq <- c("equations_single", "Equations")
 vars_sgl <- c("variables_single", "Variables (single)")
 vars_disc <- c("variables_discrete", "Variables (discrete)")
-obj <- c("obj_val", "Objective value")
+obj <- c("obj_val", "Objective value [USD]")
 time <- c("time_toSolve", "Solution time [s]")
+obj_reduced <- c("obj_val_reduced", "Objective value (outliers removed) [USD]")
+time_reduced <- c("time_toSolve_reduced", "Solution time (outliers removed) [s]")
 
 ## Combine display names into a dataframe
-facet_names <- rbind(eq, vars_sgl, vars_disc, obj, time)
+facet_names <- rbind(eq, vars_sgl, vars_disc, obj, time, obj_reduced, time_reduced)
 colnames(facet_names) <- c("stat_unordered", "stat_name")
 
 ## Merge display names to the data dataframe
 data <- merge(data, facet_names)
 
-###--------------------------------------------------
-### Order the factors for plotting order
-###--------------------------------------------------
-
+## Order the factors for plotting order
 data$stat <- factor(data$stat_name,
-				 levels=c("Equations", "Variables (single)", "Variables (discrete)", "Objective value", "Solution time [s]"))
+				 levels=c("Equations", "Variables (single)", "Variables (discrete)", "Objective value [USD]", "Objective value (outliers removed) [USD]", "Solution time [s]", "Solution time (outliers removed) [s]"))
 
 data$Formulation <- factor(data$formulation_unordered,
 				 levels=c("Null", "PRaQ", "Nonlinear"))
 
 ###--------------------------------------------------
-### Make a cleaned version of data, removing outliers
-###--------------------------------------------------
-
-## Remove outliers, and created separate dataframes
-data_cleaned <- filter(data, !conversion %in% c(6,8))
-data$adjusted <- "Outliers included"
-data_cleaned$adjusted <- "Outliers removed"
-data_all <- rbind(data, data_cleaned)
-
-## Extract objective value row from cleaned dataframe, and rename to note removal of outliers
-outliersRemoved_objVal <- filter(data_cleaned, stat_unordered=="obj_val")
-outliersRemoved_objVal$stat <- "Objective value (outliers removed)"
-
-## Extract soluton time row from cleaned dataframe, and rename to note removal of outliers
-outliersRemoved_solTime <- filter(data_cleaned, stat_unordered=="time_toSolve")
-outliersRemoved_solTime$stat <- "Solution time (outliers removed)"
-
-## Bind original dataframe, with the newly created outliers removed data
-data <- rbind(data, outliersRemoved_objVal, outliersRemoved_solTime)
-
-## OPTIONAL: remove Nonlinear information, to allow Null/PRaQ comparison
-#data <- filter(data, Formulation!="Nonlinear")
-
-###--------------------------------------------------
-### Plot data
+### Plot
 ###--------------------------------------------------
 
 plot_conversion <- ggplot(data, aes(conversion, value)) +
   geom_line(aes(colour=Formulation)) +
   facet_wrap(~stat, scales="free_y", ncol=1) +
-#  facet_grid(adjusted~stat, scales="free") +
   theme_bw() +
   theme(axis.title.y=element_blank()) +
   xlab("Number of conversion processes")
-
-plot_conversion_cleaned <- ggplot(data_cleaned, aes(conversion, value)) +
-  geom_line(aes(colour=Formulation)) +
-  facet_wrap(~stat, scales="free_y", ncol=1) +
-  theme_bw()
 
 plot_resources <- ggplot(data, aes(resources, value)) +
   geom_line(aes(colour=Formulation)) +
